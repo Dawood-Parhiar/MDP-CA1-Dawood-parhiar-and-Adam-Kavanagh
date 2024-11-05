8677f124f67eb71cd173897da835f1e26266180b
Dawood Parhiar <D00248313@student.dkit.ie>
2024-11-05 10:48:12 +0000

    Setting up the SFML

--- MDP_CA1/Main.cpp
+++ MDP_CA1/Main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Graphics.hpp>
 
 #include <iostream>
 
@@ -5,5 +6,21 @@ int main(int argc, char* argv[])
 {
     //This is going to be our Multiplayer game in c++ using sfml
     std::cout << "Hello World!" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Multiplayer Game");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    window.setFramerateLimit(60);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
 }
