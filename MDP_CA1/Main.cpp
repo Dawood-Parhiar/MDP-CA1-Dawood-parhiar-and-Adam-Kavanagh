910dcad42ceaf0e6ef4cd702fd67ec23792189e7
Dawood Parhiar <D00248313@student.dkit.ie>
2024-11-05 09:52:03 +0000

    Initial Commit of setting the project

---
+++ MDP_CA1/Main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    //This is going to be our Multiplayer game in c++ using sfml
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
