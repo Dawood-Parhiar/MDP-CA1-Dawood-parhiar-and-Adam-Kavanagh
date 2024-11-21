b6500c2d7576e108627625dc32d1af98f09fb104
Dawood Parhiar <D00248313@student.dkit.ie>
2024-11-21 15:33:35 +0000

    SFML Working

--- MDP_CA1/Main.cpp
+++ MDP_CA1/Main.cpp
@@ -1,9 +1,37 @@
-
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main(int argc, char* argv[])
+
+void sampleWindowSFML()
+{
+    // Create a window with a circle
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+    
+}
+int main()
 {
     //This is going to be our Multiplayer game in c++ using sfml
     std::cout << "Hello World!" << std::endl;
+    sampleWindowSFML();
     return 0;
 }
+
