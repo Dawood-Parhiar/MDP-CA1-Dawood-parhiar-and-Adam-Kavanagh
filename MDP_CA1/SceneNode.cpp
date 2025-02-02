e69c680b5c238f364189cac2edb09e0dd555136a
Dawood Parhiar <D00248313@student.dkit.ie>
2025-02-02 22:26:43 +0000

    Trannsparent outline for bounding rects in SceneNode

--- MDP_CA1/SceneNode.cpp
+++ MDP_CA1/SceneNode.cpp
@@ -72,7 +72,7 @@ void SceneNode::DrawBoundingRect(sf::RenderTarget& target, sf::RenderStates stat
     shape.setPosition(sf::Vector2f(rect.left, rect.top));
     shape.setSize(sf::Vector2f(rect.width, rect.height));
     shape.setFillColor(sf::Color::Transparent);
-    shape.setOutlineColor(sf::Color::Green);
+    shape.setOutlineColor(sf::Color::Transparent);
     shape.setOutlineThickness(1.f);
     target.draw(shape);
 }
